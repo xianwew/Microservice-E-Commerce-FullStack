d776d5544f97335ae694c68e73196a3c9993272d
xianwew <50687225+xianwew@users.noreply.github.com>
2024-05-28 18:49:49 -0400

    Update Rating.java

--- backend/XianweiECommerce/src/main/java/com/example/XianweiECommerce/model/Rating.java
+++ backend/XianweiECommerce/src/main/java/com/example/XianweiECommerce/model/Rating.java
@@ -28,25 +28,6 @@ public class Rating extends BaseEntity {
     @Column(name = "num_ratings", nullable = false)
     private int numRatings;
 
-//    @Column(name = "created_at", nullable = false, updatable = false)
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date createdAt;
-//
-//    @Column(name = "updated_at", nullable = false)
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date updatedAt;
-
-//    @PrePersist
-//    protected void onCreate() {
-//        createdAt = new Date();
-//        updatedAt = new Date();
-//    }
-//
-//    @PreUpdate
-//    protected void onUpdate() {
-//        updatedAt = new Date();
-//    }
-
     public enum EntityType {
         PRODUCT, SELLER
     }
