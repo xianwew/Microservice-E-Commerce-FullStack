38727d2e02f769f1e48e5e3a8532453bef3e118f
xianwew <50687225+xianwew@users.noreply.github.com>
2024-06-15 00:20:27 -0400

    backup

--- backend/Microservice/Gateway/src/main/java/com/xianwei/gatewayserver/GatewayApplication.java
+++ backend/Microservice/Gateway/src/main/java/com/xianwei/gatewayserver/GatewayApplication.java
@@ -8,9 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -27,22 +30,27 @@ public class GatewayApplication {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
 		return routeLocatorBuilder.routes()
-				.route(p -> p
-						.path("/api/user/**", "/api/cart/**", "/api/feedback/**", "/api/ratings/**")
+				.route("users_route", r -> r.path("/api/user/**", "/api/cart/**", "/api/feedback/**", "/api/ratings/**")
+						.filters(f -> f.circuitBreaker(c -> c.setName("usersCircuitBreaker").setFallbackUri("forward:/contactSupport")))
 						.uri("lb://USER"))
-				.route(p -> p
-						.path("/api/item/**", "/api/categories/**", "/api/admin/**")
+				.route("items_route", r -> r.path("/api/item/**", "/api/categories/**", "/api/admin/**")
+						.filters(f -> f.circuitBreaker(c -> c.setName("itemsCircuitBreaker").setFallbackUri("forward:/contactSupport")))
 						.uri("lb://ITEM"))
-				.route(p -> p
-						.path("/api/orders/**")
+				.route("orders_route", r -> r.path("/api/orders/**")
+						.filters(f -> f.circuitBreaker(c -> c.setName("ordersCircuitBreaker").setFallbackUri("forward:/contactSupport"))
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(userKeyResolver())))
 						.uri("lb://ORDER"))
-				.route(p -> p
-						.path("/api/payment/**")
+				.route("payment_route", r -> r.path("/api/payment/**")
+						.filters(f -> f.circuitBreaker(c -> c.setName("paymentCircuitBreaker").setFallbackUri("forward:/contactSupport")))
 						.uri("lb://PAYMENT"))
-				.route(p -> p
-						.path("/realms/**", "/protocol/**")
+				.route("keycloak_route", r -> r.path("/realms/**", "/protocol/**")
 						.uri(keycloakUrl))
 				.build();
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1, 1);
 	}
 
 	@Bean
@@ -51,5 +59,11 @@ public class GatewayApplication {
 				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
 				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(10)).build())
 				.build());
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("order"))
+				.defaultIfEmpty("anonymous");
 	}
 }
